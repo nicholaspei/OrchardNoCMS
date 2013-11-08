3e111811d095e5316dff33c421696ebf5a4423e8
nic_pei <nic162534@gmail.com>
2013-11-08 14:56:26 +0800

    去掉几个Filter，以后加上

--- src/Orchard/Security/SecurityFilter.cs
+++ src/Orchard/Security/SecurityFilter.cs
@@ -6,39 +6,40 @@ using Orchard.Mvc.Filters;
 using Orchard.UI.Admin;
 
 namespace Orchard.Security {
-    [UsedImplicitly]
-    public class SecurityFilter : FilterProvider, IExceptionFilter, IAuthorizationFilter {
-        private readonly IAuthorizer _authorizer;
+    //**以后会启用该过滤条件的**//
+    //[UsedImplicitly]
+    //public class SecurityFilter : FilterProvider, IExceptionFilter, IAuthorizationFilter {
+    //    private readonly IAuthorizer _authorizer;
 
-        public SecurityFilter(IAuthorizer authorizer) {
-            _authorizer = authorizer;
-            Logger = NullLogger.Instance;
-        }
+    //    public SecurityFilter(IAuthorizer authorizer) {
+    //        _authorizer = authorizer;
+    //        Logger = NullLogger.Instance;
+    //    }
 
-        public ILogger Logger { get; set; }
+    //    public ILogger Logger { get; set; }
 
-        public void OnAuthorization(AuthorizationContext filterContext) {
+    //    public void OnAuthorization(AuthorizationContext filterContext) {
 
-            var accessFrontEnd = filterContext.ActionDescriptor.GetCustomAttributes(typeof (AlwaysAccessibleAttribute), true).Any();
+    //        var accessFrontEnd = filterContext.ActionDescriptor.GetCustomAttributes(typeof (AlwaysAccessibleAttribute), true).Any();
 
-            if (!AdminFilter.IsApplied(filterContext.RequestContext) && !accessFrontEnd && !_authorizer.Authorize(StandardPermissions.AccessFrontEnd)) {
-                filterContext.Result = new HttpUnauthorizedResult();
-            }
-        }
+    //        if (!AdminFilter.IsApplied(filterContext.RequestContext) && !accessFrontEnd && !_authorizer.Authorize(StandardPermissions.AccessFrontEnd)) {
+    //            filterContext.Result = new HttpUnauthorizedResult();
+    //        }
+    //    }
 
-        public void OnException(ExceptionContext filterContext) {
-            if (!(filterContext.Exception is OrchardSecurityException))
-                return;
+    //    public void OnException(ExceptionContext filterContext) {
+    //        if (!(filterContext.Exception is OrchardSecurityException))
+    //            return;
 
-            try {
-                Logger.Information(filterContext.Exception, "Security exception converted to access denied result");
-            }
-            catch {
-                //a logger exception can't be allowed to interrupt this process
-            }
+    //        try {
+    //            Logger.Information(filterContext.Exception, "Security exception converted to access denied result");
+    //        }
+    //        catch {
+    //            //a logger exception can't be allowed to interrupt this process
+    //        }
 
-            filterContext.Result = new HttpUnauthorizedResult();
-            filterContext.ExceptionHandled = true;
-        }
-    }
+    //        filterContext.Result = new HttpUnauthorizedResult();
+    //        filterContext.ExceptionHandled = true;
+    //    }
+    //}
 }
--- src/Orchard/UI/Navigation/MenuFilter.cs
+++ src/Orchard/UI/Navigation/MenuFilter.cs
@@ -7,64 +7,65 @@ using Orchard.Mvc.Filters;
 using Orchard.UI.Admin;
 
 namespace Orchard.UI.Navigation {
-    public class MenuFilter : FilterProvider, IResultFilter {
-        private readonly INavigationManager _navigationManager;
-        private readonly IWorkContextAccessor _workContextAccessor;
-        private readonly dynamic _shapeFactory;
+    //** 无效掉MenuFiler，以后会启用 **//
+    //public class MenuFilter : FilterProvider, IResultFilter {
+    //    private readonly INavigationManager _navigationManager;
+    //    private readonly IWorkContextAccessor _workContextAccessor;
+    //    private readonly dynamic _shapeFactory;
 
-        public MenuFilter(INavigationManager navigationManager,
-            IWorkContextAccessor workContextAccessor,
-            IShapeFactory shapeFactory) {
+    //    public MenuFilter(INavigationManager navigationManager,
+    //        IWorkContextAccessor workContextAccessor,
+    //        IShapeFactory shapeFactory) {
 
-            _navigationManager = navigationManager;
-            _workContextAccessor = workContextAccessor;
-            _shapeFactory = shapeFactory;
-        }
+    //        _navigationManager = navigationManager;
+    //        _workContextAccessor = workContextAccessor;
+    //        _shapeFactory = shapeFactory;
+    //    }
 
-        public void OnResultExecuting(ResultExecutingContext filterContext) {
-            // should only run on a full view rendering result
-            if (!(filterContext.Result is ViewResult)) {
-                return;
-            }
+    //    public void OnResultExecuting(ResultExecutingContext filterContext) {
+    //        // should only run on a full view rendering result
+    //        if (!(filterContext.Result is ViewResult)) {
+    //            return;
+    //        }
 
-            WorkContext workContext = _workContextAccessor.GetContext(filterContext);
+    //        WorkContext workContext = _workContextAccessor.GetContext(filterContext);
 
-            const string menuName = "admin";
-            if (!AdminFilter.IsApplied(filterContext.RequestContext)) {
-                return;
-            }
+    //        const string menuName = "admin";
+    //        if (!AdminFilter.IsApplied(filterContext.RequestContext)) {
+    //            return;
+    //        }
 
-            IEnumerable<MenuItem> menuItems = _navigationManager.BuildMenu(menuName);
+    //        IEnumerable<MenuItem> menuItems = _navigationManager.BuildMenu(menuName);
 
-            // adding query string parameters
-            var routeData = new RouteValueDictionary(filterContext.RouteData.Values);
-            var queryString = workContext.HttpContext.Request.QueryString;
-            if (queryString != null) {
-                foreach (var key in from string key in queryString.Keys where key != null && !routeData.ContainsKey(key) let value = queryString[key] select key) {
-                    routeData[key] = queryString[key];
-                }
-            }
+    //        // adding query string parameters
+    //        var routeData = new RouteValueDictionary(filterContext.RouteData.Values);
+    //        var queryString = workContext.HttpContext.Request.QueryString;
+    //        if (queryString != null) {
+    //            foreach (var key in from string key in queryString.Keys where key != null && !routeData.ContainsKey(key) let value = queryString[key] select key) {
+    //                routeData[key] = queryString[key];
+    //            }
+    //        }
 
-            // Set the currently selected path
-            Stack<MenuItem> selectedPath = NavigationHelper.SetSelectedPath(menuItems, workContext.HttpContext.Request, routeData);
+    //        // Set the currently selected path
+    //        Stack<MenuItem> selectedPath = NavigationHelper.SetSelectedPath(menuItems, workContext.HttpContext.Request, routeData);
 
-            // Populate main nav
-            dynamic menuShape = _shapeFactory.Menu().MenuName(menuName);
-            NavigationHelper.PopulateMenu(_shapeFactory, menuShape, menuShape, menuItems);
+    //        // Populate main nav
+    //        dynamic menuShape = _shapeFactory.Menu().MenuName(menuName);
+    //        NavigationHelper.PopulateMenu(_shapeFactory, menuShape, menuShape, menuItems);
 
-            // Add any know image sets to the main nav
-            IEnumerable<string> menuImageSets = _navigationManager.BuildImageSets(menuName);
-            if (menuImageSets != null && menuImageSets.Any())
-                menuShape.ImageSets(menuImageSets);
+    //        // Add any know image sets to the main nav
+    //        IEnumerable<string> menuImageSets = _navigationManager.BuildImageSets(menuName);
+    //        if (menuImageSets != null && menuImageSets.Any())
+    //            menuShape.ImageSets(menuImageSets);
 
-            workContext.Layout.Navigation.Add(menuShape);
+    //        workContext.Layout.Navigation.Add(menuShape);
 
-            // Populate local nav
-            dynamic localMenuShape = _shapeFactory.LocalMenu().MenuName(string.Format("local_{0}", menuName));
-            NavigationHelper.PopulateLocalMenu(_shapeFactory, localMenuShape, localMenuShape, selectedPath);
-            workContext.Layout.LocalNavigation.Add(localMenuShape);
-        }
+    //        // Populate local nav
+    //        dynamic localMenuShape = _shapeFactory.LocalMenu().MenuName(string.Format("local_{0}", menuName));
+    //        NavigationHelper.PopulateLocalMenu(_shapeFactory, localMenuShape, localMenuShape, selectedPath);
+    //        workContext.Layout.LocalNavigation.Add(localMenuShape);
+    //    }
 
-        public void OnResultExecuted(ResultExecutedContext filterContext) { }
-    }
+    //    public void OnResultExecuted(ResultExecutedContext filterContext) { }
+    //}
 }

1cbcb0fb989bf03ce9f1b12cf19533aa3f3b41b8
nic_pei <nic162534@gmail.com>
2013-11-08 13:38:02 +0800

    add the setting module ,framework work now
    非常兴奋啊，终于可以运转了。。

---
+++ src/Orchard.Web/Core/Settings/Models/SiteSettings2Part.cs
@@ -0,0 +1,12 @@
+﻿using Orchard.Data.Conventions;
+
+namespace Orchard.Core.Settings.Models {
+    public sealed class SiteSettings2Part  {
+        [StringLengthMax]
+        public string BaseUrl
+        {
+            get;
+            set;
+        }
+    }
+}
---
+++ src/Orchard.Web/Core/Settings/Models/SiteSettings2PartRecord.cs
@@ -0,0 +1,10 @@
+﻿using Orchard.Data.Conventions;
+using System;
+
+namespace Orchard.Core.Settings.Models {
+    public class SiteSettings2PartRecord  {
+        [StringLengthMax]
+        public virtual Guid Id { get; set; }
+        public virtual string BaseUrl { get; set; }
+    }
+}

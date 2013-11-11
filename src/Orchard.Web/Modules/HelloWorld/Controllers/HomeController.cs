c6ed2c5fc3537ba1fce3da2c35dd450a1ef5b908
nic_pei <nic162534@gmail.com>
2013-11-11 09:49:53 +0800

    another module :Orchard.Modules

---
+++ src/Orchard.Web/Modules/HelloWorld/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Orchard.Themes;
+using System.Web.Mvc;
+
+namespace HelloWorld.Controllers
+{
+     [Themed]
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}

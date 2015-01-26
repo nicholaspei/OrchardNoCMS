af76ad1aa8f70fae22b0d11c1924c8d57e1f8c15
nicholaspei <nic162534@gmail.com>
2015-01-26 16:35:12 +0800

    update jetBrains.Annotations

--- src/OrchardVNext/Validation/Argument.cs
+++ src/OrchardVNext/Validation/Argument.cs
@@ -1,48 +1,63 @@
 ﻿using System;
 using JetBrains.Annotations;
 
-namespace OrchardVNext.Validation {
-    public class Argument {
-        [AssertionMethod]
-        public static void Validate([AssertionCondition(AssertionConditionType.IS_TRUE)] bool condition, [InvokerParameterName]string name) {
-            if (!condition) {
-                throw new ArgumentException("Invalid argument", name);
-            }
-        }
+namespace OrchardVNext.Validation
+{
+	public class Argument
+	{
+		[ContractAnnotation("halt <= condition: true")]
+		public static void Validate(bool condition, [InvokerParameterName]string name)
+		{
+			if (!condition)
+			{
+				throw new ArgumentException("Invalid argument", name);
+			}
+		}
 
-        [AssertionMethod]
-        public static void Validate([AssertionCondition(AssertionConditionType.IS_TRUE)]bool condition, [InvokerParameterName]string name, string message) {
-            if (!condition) {
-                throw new ArgumentException(message, name);
-            }
-        }
+		[ContractAnnotation("halt <= condition: true")]
+		public static void Validate(bool condition, [InvokerParameterName]string name, string message)
+		{
+			if (!condition)
+			{
+				throw new ArgumentException(message, name);
+			}
+		}
 
-        [AssertionMethod]
-        public static void ThrowIfNull<T>([AssertionCondition(AssertionConditionType.IS_NOT_NULL)]T value, [InvokerParameterName]string name) where T : class {
-            if (value == null) {
-                throw new ArgumentNullException(name);
-            }
-        }
 
-        [AssertionMethod]
-        public static void ThrowIfNull<T>([AssertionCondition(AssertionConditionType.IS_NOT_NULL)]T value, [InvokerParameterName]string name, string message) where T : class {
-            if (value == null) {
-                throw new ArgumentNullException(name, message);
-            }
-        }
+		[ContractAnnotation("value:null => fail")]
+		public static void ThrowIfNull<T>(T value, [InvokerParameterName]string name) where T : class
+		{
+			if (value == null)
+			{
+				throw new ArgumentNullException(name);
+			}
+		}
 
-        [AssertionMethod]
-        public static void ThrowIfNullOrEmpty([AssertionCondition(AssertionConditionType.IS_NOT_NULL)]string value, [InvokerParameterName]string name) {
-            if (string.IsNullOrEmpty(value)) {
-                throw new ArgumentException("Argument must be a non empty string", name);
-            }
-        }
+		[ContractAnnotation("value:null => fail")]
+		public static void ThrowIfNull<T>(T value, [InvokerParameterName]string name, string message) where T : class
+		{
+			if (value == null)
+			{
+				throw new ArgumentNullException(name, message);
+			}
+		}
 
-        [AssertionMethod]
-        public static void ThrowIfNullOrEmpty([AssertionCondition(AssertionConditionType.IS_NOT_NULL)]string value, [InvokerParameterName]string name, string message) {
-            if (string.IsNullOrEmpty(value)) {
-                throw new ArgumentException(message, name);
-            }
-        }
-    }
+		[ContractAnnotation("value:null => fail")]
+		public static void ThrowIfNullOrEmpty(string value, [InvokerParameterName]string name)
+		{
+			if (string.IsNullOrEmpty(value))
+			{
+				throw new ArgumentException("Argument must be a non empty string", name);
+			}
+		}
+
+		[ContractAnnotation("value:null => fail")]
+		public static void ThrowIfNullOrEmpty(string value, [InvokerParameterName]string name, string message)
+		{
+			if (string.IsNullOrEmpty(value))
+			{
+				throw new ArgumentException(message, name);
+			}
+		}
+	}
 }
